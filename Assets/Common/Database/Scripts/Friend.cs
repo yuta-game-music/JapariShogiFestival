6278c364d41d96d35d8e9b3f4086dc0ec914ea84
Yuta <yunaomaki708@gmail.com>
2022-01-22 18:35:55 +0900

    fixed Vector2->Vector2Int, set MoveNormal virtual method

--- Assets/Common/Database/Scripts/Friend.cs
+++ Assets/Common/Database/Scripts/Friend.cs
@@ -11,8 +11,15 @@ namespace JSF.Database
     {
         public abstract string Name { get; }
         public abstract Dictionary<Vector2,MovementSettings> GetMovementSheet();
-        public abstract IEnumerator MoveNormal(Vector2 from, Vector2 to, FriendOnBoard friendsOnBoard);
-        public abstract IEnumerator MoveWithSkill(Vector2 from, Vector2 to, FriendOnBoard friendsOnBoard);
+        public virtual IEnumerator MoveNormal(Vector2Int from, Vector2Int to, FriendOnBoard friendsOnBoard)
+        {
+            friendsOnBoard.Animator.SetBool("MoveForward", true);
+            int layer_id = friendsOnBoard.Animator.GetLayerIndex("Movement");
+            yield return new WaitUntil(() => friendsOnBoard.Animator.GetCurrentAnimatorStateInfo(layer_id).IsName("WaitingForControl"));
+            friendsOnBoard.BoardManager.MoveFriend(friendsOnBoard,to);
+            friendsOnBoard.Animator.SetBool("MoveForward", false);
+        }
+        public abstract IEnumerator MoveWithSkill(Vector2Int from, Vector2Int to, FriendOnBoard friendsOnBoard);
 
         public Sprite OnBoardImage;
 
