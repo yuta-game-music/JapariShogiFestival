5dc42b2ce676cc7fb1f7ec2e1340e994570d9a6d
Yuta <yunaomaki708@gmail.com>
2022-01-27 11:15:52 +0900

    changed user format

--- Assets/Common/GlobalVariables/GlobalVariable.cs
+++ Assets/Common/GlobalVariables/GlobalVariable.cs
@@ -2,6 +2,8 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using JSF.Game.Player;
+using JSF.Database;
+using JSF.Database.Friends;
 
 namespace JSF
 {
@@ -19,22 +21,42 @@ namespace JSF
 
         #region PlayerInfo
         public static PlayerInfo[] Players = new PlayerInfo[]{
-            new PlayerInfo("Player1"),
-            new PlayerInfo("Player2")
+            new PlayerInfo(0,"Player1",PlayerType.User,Color.magenta,null),
+            new PlayerInfo(1,"Player2",PlayerType.User,Color.blue,null)
         };
         #endregion
 
+        #region GameResultInfo
+        public static PlayerInfo? Winner;
+        #endregion
     }
 
-    public struct PlayerInfo
+    public struct PlayerInfo : IEqualityComparer<PlayerInfo>
     {
+        public int ID;
         public string Name;
         public PlayerType PlayerType;
+        public Color PlayerColor;
+        public Friend Leader { get => (Friends!=null && Friends.Length>0) ? Friends[0] : null; }
+        public Friend[] Friends;
 
-        public PlayerInfo(string name="Player", PlayerType playerType = PlayerType.User)
+        public PlayerInfo(int id, string name, PlayerType playerType, Color playerColor, Friend[] friends)
         {
+            ID = id;
             Name = name;
             PlayerType = playerType;
+            PlayerColor = playerColor;
+            Friends = friends;
+        }
+
+        public bool Equals(PlayerInfo x, PlayerInfo y)
+        {
+            return x.ID==y.ID;
+        }
+
+        public int GetHashCode(PlayerInfo obj)
+        {
+            return obj.ID;
         }
     }
 
