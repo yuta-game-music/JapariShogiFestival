2ba4ee3d6fe7974dbe628515ef7fc8709139be2b
Yuta <yunaomaki708@gmail.com>
2022-01-27 11:14:58 +0900

    added Result Page

---
+++ Assets/ResultPage/Scripts/ResultPageController.cs
@@ -0,0 +1,36 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace JSF.Result
+{
+    public class ResultPageController : MonoBehaviour
+    {
+        public PlayerViewController PlayerViewController1;
+        public PlayerViewController PlayerViewController2;
+
+        private bool _init = false;
+        // Start is called before the first frame update
+        void Start()
+        {
+            Init();
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+            Init();
+        }
+
+        private void Init()
+        {
+            if (!_init)
+            {
+                PlayerViewController1.PlayerInfo = GlobalVariable.Players[0];
+                PlayerViewController2.PlayerInfo = GlobalVariable.Players[1];
+                _init = true;
+            }
+        }
+    }
+
+}
