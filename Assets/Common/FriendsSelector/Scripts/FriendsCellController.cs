3c66f6e2fe423a7867cff0dcc91ea31eb2c6b3a6
Yuta <yunaomaki708@gmail.com>
2022-01-28 21:58:27 +0900

    tuned friends-selector UI scaling

--- Assets/Common/FriendsSelector/Scripts/FriendsCellController.cs
+++ Assets/Common/FriendsSelector/Scripts/FriendsCellController.cs
@@ -9,10 +9,24 @@ namespace JSF.Common.FriendsSelector
     public class FriendsCellController : UI.Button
     {
         public Image ThumbImage;
+        public RectTransform ThumbTF;
         public TMPro.TMP_Text Text;
+        public RectTransform TextTF;
         public Friend Friend { set => SetFriend(value); }
         private Friend _friend;
         public FriendsSelectorController Controller;
+        private RectTransform TF;
+
+        private void Start()
+        {
+            TF = GetComponent<RectTransform>();
+        }
+
+        private void Update()
+        {
+            TF.sizeDelta = Vector2.one * TF.rect.size.y;
+            
+        }
 
         private void SetFriend(Friend friend)
         {
