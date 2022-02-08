a613b33a17659085fccee4ebdf09a409e4c057a7
Yuta <yunaomaki708@gmail.com>
2022-02-08 21:24:50 +0900

    added function for debugging

--- Assets/ResultPage/Scripts/ResultPageController.cs
+++ Assets/ResultPage/Scripts/ResultPageController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using JSF.Common.PlayerView;
 using JSF.Common.UI;
+using System;
 
 namespace JSF.Result
 {
@@ -18,6 +19,22 @@ namespace JSF.Result
         void Start()
         {
             Init();
+#if UNITY_EDITOR
+            // TODO: デバッグ用のため削除
+            /*
+            for(var i = 0; i < 2; i++)
+            {
+                var strategy = GlobalVariable.Players[i].CPUStrategy = new Game.CPU.CPUStrategy()
+                {
+                    Overall = (Game.CPU.CPUStrategyOverall)UnityEngine.Random.Range(0, Enum.GetValues(typeof(Game.CPU.CPUStrategyOverall)).Length),
+                    Select = Game.CPU.CPUStrategySelect.NearestToLeader,//(Game.CPU.CPUStrategySelect)UnityEngine.Random.Range(0, Enum.GetValues(typeof(Game.CPU.CPUStrategySelect)).Length),
+                    Move = Game.CPU.CPUStrategyMove.ApproachToLeader//(Game.CPU.CPUStrategyMove)UnityEngine.Random.Range(0, Enum.GetValues(typeof(Game.CPU.CPUStrategyMove)).Length),
+                };
+                Debug.Log("Player"+(i+1)+" "+strategy.Overall+" "+strategy.Select+" "+strategy.Move);
+            }
+            UnityEngine.SceneManagement.SceneManager.LoadScene("MainBoard");
+            */
+#endif
         }
 
         // Update is called once per frame
